6cb22993cfc611f0312fb78de26abe7a88056830
kistitan <kistitan@users.noreply.github.com>
2019-05-07 21:42:42 +0200

    App navigator - in progress

---
+++ FetLife/fetlife/src/main/java/com/bitlove/fetlife/common/navigation/AppNavigator.kt
@@ -0,0 +1,15 @@
+package com.bitlove.fetlife.common.navigation
+
+import android.content.Context
+
+class AppNavigator {
+
+    fun startInbox(context: Context) {
+
+    }
+
+    fun startFeed(context: Context) {
+
+    }
+
+}
